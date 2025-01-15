d184046456b8ee7bbd499624153bfa7415ac487b
Jeff-Xu23 <jeffery.xu@aelf.io>
2025-01-15 16:40:40 +0800

    feat:modify social chat agent

--- src/AiSmart.GAgent.SocialGAgent/GAgent/ISocialGAgent.cs
+++ src/AiSmart.GAgent.SocialGAgent/GAgent/ISocialGAgent.cs
@@ -1,6 +1,6 @@
 using Aevatar.GAgents.MicroAI.Agent;
 
-namespace Aevatar.GAgents.SocialAgent.GAgent;
+namespace Aevatar.GAgents.SocialChat.GAgent;
 
 public interface ISocialGAgent:IMicroAIGAgent
 {
