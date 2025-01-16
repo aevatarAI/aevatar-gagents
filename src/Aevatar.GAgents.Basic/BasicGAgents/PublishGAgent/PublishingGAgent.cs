53575429a380a4f8be7656a9813613c629dd2217
jason20240511 <jason.zhang@aelf.io>
2025-01-16 12:00:19 +0800

    Migrated PublishGAgent to the new repository structure

---
+++ src/Aevatar.GAgents.Basic/BasicGAgents/PublishGAgent/PublishingGAgent.cs
@@ -0,0 +1,34 @@
+using Aevatar.Core;
+using Aevatar.Core.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Aevatar.GAgents.Basic.PublishGAgent;
+
+[GenerateSerializer]
+public class PublishingAgentState : StateBase
+{
+}
+
+[GAgent("publishing")]
+public class PublishingGAgent : GAgentBase<PublishingAgentState, PublishingStateLogEvent>, IPublishingGAgent
+{
+    public PublishingGAgent(ILogger<PublishingGAgent> logger) : base(logger)
+    {
+    }
+
+    public override Task<string> GetDescriptionAsync()
+    {
+        return Task.FromResult("Agent to be used for publishing new events.");
+    }
+
+    public async Task PublishEventAsync<T>(T @event) where T : EventBase
+    {
+        if (@event == null)
+        {
+            throw new ArgumentNullException(nameof(@event));
+        }
+
+        Logger.LogInformation($"PublishingAgent publish {@event}");
+        await PublishAsync(@event);
+    }
+}
