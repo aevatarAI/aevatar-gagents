8f2657039110ff95dc62b027f3f56c415e148cbf
Jeff-Xu23 <jeffery.xu@aelf.io>
2025-02-26 19:02:14 +0800

    feat:init twitter reply agents

--- src/Aevatar.GAgents.Twitter/GAgents/TwitterGAgentState.cs
+++ src/Aevatar.GAgents.Twitter/GAgents/TwitterGAgentState.cs
@@ -14,7 +14,7 @@ public class TwitterGAgentState : StateBase
     [Id(1)] public string UserId { get; set; }
     [Id(2)] public string Token { get; set; }
     [Id(3)] public string TokenSecret { get; set; }
-    [Id(4)] public Dictionary<string, string> RepliedTweets { get; set; }
+    [Id(4)] public Dictionary<string, string> RepliedTweets { get; set; } = new Dictionary<string, string>();
     [Id(5)] public string UserName { get; set; }
     [Id(6)] public List<Guid> SocialRequestList { get; set; } = new List<Guid>();
     [Id(8)] public string ConsumerKey { get; set; }
