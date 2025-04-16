d39ec729f5ff9c3053d772306466e22e071fa686
jason20240511 <jason.zhang@aelf.io>
2025-04-16 13:18:27 +0800

    update AIStreamingResponseGEvent to signalr event

--- src/Aevatar.GAgents.AIGAgent/GEvents/AIStreamingResponseGEvent.cs
+++ src/Aevatar.GAgents.AIGAgent/GEvents/AIStreamingResponseGEvent.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel;
 using Aevatar.Core.Abstractions;
 using Aevatar.GAgents.AIGAgent.Dtos;
+using Aevatar.SignalR;
 using Orleans;
 using Orleans.Runtime;
 
@@ -9,7 +10,7 @@ namespace Aevatar.GAgents.AIGAgent.GEvents;
 
 [Description("Return a streaming chunk")]
 [GenerateSerializer]
-public class AIStreamingResponseGEvent : EventBase
+public class AIStreamingResponseGEvent : ResponseToPublisherEventBase
 {
     [Id(0)] public string ResponseContent { get; set; }
     [Id(1)] public int SerialNumber { get; set; }
@@ -19,7 +20,7 @@ public class AIStreamingResponseGEvent : EventBase
 
 [Description("Return a error reponse")]
 [GenerateSerializer]
-public class AIStreamingErrorResponseGEvent : EventBase
+public class AIStreamingErrorResponseGEvent : ResponseToPublisherEventBase
 {
     [Id(0)] public AIChatContextDto Context { get; set; } = new();
     
