d516d79d21075eeace362f430fae58731028332c
chao0kang <chao.kang@aelf.io>
2025-07-18 15:09:10 +0800

    feat: add workflowViewGAgent

--- src/Aevatar.GAgents.GroupChat/GAgent/Coordinator/WorkflowView/Dto/WorkflowViewConfigDto.cs
+++ src/Aevatar.GAgents.GroupChat/GAgent/Coordinator/WorkflowView/Dto/WorkflowViewConfigDto.cs
@@ -16,8 +16,9 @@ public class WorkflowNodeDto
     [Id(0)] public string AgentType { get; set; }
     [Id(1)] public string Name { get; set; }
     [Id(2)] public Dictionary<string,string> ExtendedData { get; set; } = new();
-    [Id(3)] public Dictionary<string, object>? Properties { get; set; } = new();
+    [Id(3)] public Dictionary<string, object> Properties { get; set; } = new();
     [Id(4)] public Guid NodeId { get; set; }
+    [Id(5)] public Guid AgentId { get; set; }
 }
 
 [GenerateSerializer]
---
+++ src/Aevatar.GAgents.GroupChat/GAgent/Coordinator/WorkflowView/WorkflowViewGAgent.cs
@@ -0,0 +1,184 @@
+using System.Configuration;
+using System.Reflection;
+using Aevatar.Core;
+using Aevatar.Core.Abstractions;
+using Aevatar.GAgents.GroupChat.GAgent.Coordinator.WorkflowView.Dto;
+using Aevatar.GAgents.GroupChat.GAgent.Coordinator.WorkflowView.GEvent;
+using Aevatar.GAgents.GroupChat.GAgent.Coordinator.WorkflowView.LogEvent;
+using Aevatar.GAgents.GroupChat.WorkflowCoordinator;
+using Aevatar.GAgents.GroupChat.WorkflowCoordinator.Dto;
+using Newtonsoft.Json;
+using Volo.Abp;
+
+namespace Aevatar.GAgents.GroupChat.GAgent.Coordinator.WorkflowView;
+
+[GAgent]
+public class WorkflowViewGAgent : GAgentBase<WorkflowViewState, WorkflowViewLogEvent, EventBase,
+    WorkflowViewConfigDto>, IWorkflowViewGAgent
+{
+    private readonly IGAgentFactory _gAgentFactory;
+
+    public WorkflowViewGAgent(IGAgentFactory agentFactory)
+    {
+        _gAgentFactory = agentFactory;
+    }
+
+    public override Task<string> GetDescriptionAsync()
+    {
+        return Task.FromResult("Workflow View GAgent");
+    }
+
+    [EventHandler]
+    private async Task HandlerEventAsync(CreateWorkflowGEvent @event)
+    {
+        foreach (var workflowNode in State.WorkflowNodeList)
+        {
+            var agentId = workflowNode.AgentId == Guid.Empty ? Guid.NewGuid() : workflowNode.AgentId;
+            var agentProperties =
+                workflowNode.Properties.IsNullOrEmpty() ? string.Empty : JsonConvert.SerializeObject(workflowNode.Properties);
+            await CreateOrUpdateBusinessAgent(agentId, workflowNode.AgentType, agentProperties);
+            
+            RaiseEvent(new UpdateNodeAgentIdLogEvent()
+            {
+                NodeId = workflowNode.NodeId,
+                AgentId = agentId
+            });
+        }
+        var workflowGAgentId = State.WorkflowCoordinatorGAgentId == Guid.Empty ? Guid.NewGuid() : State.WorkflowCoordinatorGAgentId;
+        var workflowCoordinatorGAgent = GrainFactory.GetGrain<IWorkflowCoordinatorGAgent>(workflowGAgentId);
+        var workflowConfig = new WorkflowCoordinatorConfigDto();
+        var nodeMap = State.WorkflowNodeList.ToDictionary(r => r.NodeId, r => r);
+        foreach (var nodeUnit in State.WorkflowNodeUnitList)
+        {
+            var curNode = nodeMap[nodeUnit.NodeId];
+            var nextNode = nodeMap[nodeUnit.NextNodeId];
+            workflowConfig.WorkflowUnitList.Add(new WorkflowUnitDto()
+            {
+                ExtendedData = curNode.ExtendedData,
+                GrainId = GrainId.Create(curNode.AgentType, GuidToGrainKey(curNode.AgentId)).ToString(),
+                NextGrainId = GrainId.Create(nextNode.AgentType, GuidToGrainKey(nextNode.AgentId)).ToString()
+            });
+        }
+        await workflowCoordinatorGAgent.ConfigAsync(workflowConfig);
+        RaiseEvent(new UpdateWorkflowAgentIdLogEvent()
+        {
+            AgentId = workflowGAgentId
+        });
+    }
+    
+    private static string GuidToGrainKey(Guid primaryKey)
+    {
+        return primaryKey.ToString("N");
+    }
+
+    private async Task CreateOrUpdateBusinessAgent(Guid primaryKey, string agentType,
+        string agentProperties)
+    {
+        var grainId = GrainId.Create(agentType, primaryKey.ToString("N"));
+        var businessAgent = await _gAgentFactory.GetGAgentAsync(grainId);
+        
+        var configurationType = await businessAgent.GetConfigurationTypeAsync();
+        if (configurationType == null || configurationType.IsAbstract)
+        {
+            return;
+        }
+
+        if (agentProperties.IsNullOrEmpty())
+        {
+            return;
+        }
+
+        var config = JsonConvert.DeserializeObject(agentProperties, configurationType) as ConfigurationBase;
+        if (config == null )
+        {
+            return;
+        }
+
+        await businessAgent.ConfigAsync(config);
+    }
+    
+    protected override async Task PerformConfigAsync(WorkflowViewConfigDto configuration)
+    {
+        await TrySaveWorkflowViewAsync(configuration);
+    }
+
+    private async Task TrySaveWorkflowViewAsync(WorkflowViewConfigDto configuration)
+    {
+        if (configuration.WorkflowNodeList.IsNullOrEmpty())
+        {
+            return;
+        }
+
+        foreach (var node in configuration.WorkflowNodeList)
+        {
+            if (node.NodeId == Guid.Empty || node.AgentType.IsNullOrEmpty() || node.Name.IsNullOrEmpty())
+            {
+                throw new ArgumentException("The workflow view node has invalid value.");
+            }
+        }
+
+        var nodeIdList = configuration.WorkflowNodeList.Select(t => t.NodeId).ToList();
+        var notExistedNodeId = configuration.WorkflowNodeUnitList.Where(t =>
+            !nodeIdList.Contains(t.NodeId) || !nodeIdList.Contains(t.NextNodeId)).ToList();
+        if (notExistedNodeId.Count > 0)
+        {
+            throw new ArgumentException("The workflow view invalid nodeId.");
+        }
+
+        var addNodeList = new List<WorkflowNodeDto>();
+        var updateNodeList = new List<WorkflowNodeDto>();
+        foreach (var node in configuration.WorkflowNodeList)
+        {
+            var stateNode = State.WorkflowNodeList.FirstOrDefault(t => t.NodeId == node.NodeId);
+            if (stateNode == null)
+            {
+                addNodeList.Add(node);
+                continue;
+            }
+            updateNodeList.Add(node);
+        }
+
+        var removeNodeIdList = State.WorkflowNodeList.Select(t => t.NodeId).Except(nodeIdList).ToList();
+        RaiseEvent(new UpdateWorkflowViewLogEvent
+        {
+            AddNodeList = addNodeList,
+            UpdateNodeList = updateNodeList,
+            RemoveNodeIdList = removeNodeIdList
+        });
+    }
+
+    protected override void GAgentTransitionState(WorkflowViewState state,
+        StateLogEventBase<WorkflowViewLogEvent> @event)
+    {
+        switch (@event)
+        {
+            case UpdateWorkflowViewLogEvent updateWorkflowViewLogEvent:
+                foreach (var removeNodeId in updateWorkflowViewLogEvent.RemoveNodeIdList)
+                {
+                    var removeNode = State.WorkflowNodeList.FirstOrDefault(t => t.NodeId == removeNodeId);
+                    if (removeNode != null)
+                    {
+                        State.WorkflowNodeList.Remove(removeNode);
+                    }
+                }
+                foreach (var node in updateWorkflowViewLogEvent.UpdateNodeList)
+                {
+                    var updateNode = State.WorkflowNodeList.FirstOrDefault(t => t.NodeId == node.NodeId);
+                    if (updateNode != null)
+                    {
+                        updateNode.Name = node.Name;
+                        updateNode.Properties = node.Properties;
+                        updateNode.ExtendedData = node.ExtendedData;
+                    }
+                }
+                State.WorkflowNodeList.AddRange(updateWorkflowViewLogEvent.AddNodeList);
+                break;
+        }
+
+        base.GAgentTransitionState(state, @event);
+    }
+}
+
+public interface IWorkflowViewGAgent : IStateGAgent<WorkflowViewState>
+{
+}
