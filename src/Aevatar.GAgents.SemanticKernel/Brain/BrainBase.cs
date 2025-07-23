3bd3431eac8a226576ba0f3e1b4b9fa224bd7a8b
zhxymh <sean.zhang@aelf.io>
2025-07-23 14:11:02 +0800

    fix: image order

--- src/Aevatar.GAgents.SemanticKernel/Brain/BrainBase.cs
+++ src/Aevatar.GAgents.SemanticKernel/Brain/BrainBase.cs
@@ -207,9 +207,9 @@ public abstract class BrainBase : IBrain
 
             await Task.WhenAll(downloadTasks);
             
-            foreach (var image in images)
+            foreach (var key in imageKeys)
             {
-                messageContentCollection.Add(new ImageContent(new ReadOnlyMemory<byte>(image.Value), ImageHelper.GetMineType(image.Key)));
+                messageContentCollection.Add(new ImageContent(new ReadOnlyMemory<byte>(images[key]), ImageHelper.GetMineType(key)));
             }
             chatHistory.AddUserMessage(messageContentCollection);
         }
