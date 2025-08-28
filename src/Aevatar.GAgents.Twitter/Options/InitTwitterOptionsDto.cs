11d1791452779b24e38e9e320d5d8f8b6d92982b
chent00ng <chen.tang@aelf.io>
2025-08-28 10:56:46 +0800

    Add documentation links to Twitter API configuration fields
    - Add DocumentationLink attribute to ConsumerKey pointing to OAuth 1.0a API key documentation
    - Add DocumentationLink attribute to BearerToken pointing to OAuth 2.0 bearer token documentation
    - Improve developer experience by providing direct links to X (Twitter) API documentation

--- src/Aevatar.GAgents.Twitter/Options/InitTwitterOptionsDto.cs
+++ src/Aevatar.GAgents.Twitter/Options/InitTwitterOptionsDto.cs
@@ -13,24 +13,22 @@ public class InitTwitterOptionsDto : ConfigurationBase
     [Id(0)]
     [Required(ErrorMessage = "Consumer Key is required")]
     [RegularExpression(@"^[A-Za-z0-9]+$", ErrorMessage = "Consumer Key can only contain alphanumeric characters")]
-    [DocumentationLink("https://developer.x.com")]
+    [DocumentationLink("https://docs.x.com/fundamentals/authentication/oauth-1-0a/api-key-and-secret")]
     public string ConsumerKey { get; set; } = "YOUR_TWITTER_API_KEY";
     
     [Id(1)]
     [Required(ErrorMessage = "Consumer Secret is required")]
     [RegularExpression(@"^[A-Za-z0-9]+$", ErrorMessage = "Consumer Secret can only contain alphanumeric characters")]
-    [DocumentationLink("https://developer.x.com")]
     public string ConsumerSecret { get; set; } = "YOUR_API_SECRET";
     
     [Id(2)]
     [Required(ErrorMessage = "Encryption Password is required")]
-    [DocumentationLink("https://developer.x.com")]
     public string EncryptionPassword { get; set; } = "YOUR_ENCRYPTION_PASSWORD";
     
     [Id(3)]
     [Required(ErrorMessage = "Bearer Token is required")]
     [RegularExpression(@"^[A-Za-z0-9_-]+$", ErrorMessage = "Bearer Token format is invalid")]
-    [DocumentationLink("https://developer.x.com")]
+    [DocumentationLink("https://docs.x.com/fundamentals/authentication/oauth-2-0/bearer-tokens")]
     public string BearerToken { get; set; } = "YOUR_BEARER_TOKEN";
     
     [Id(4)]
